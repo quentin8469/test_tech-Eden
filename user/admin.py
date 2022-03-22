7d7e3a1ddee28d1058d65f18459a738437508d77
Quentin Laparra <quentin.laparra@gmail.com>
2022-03-22 21:06:28 +0100

    add admin register for user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from user.models import CustomUser
+
+
+@admin.register(CustomUser)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("username", "email",)
