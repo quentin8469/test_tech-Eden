66e3d878def45e38de56524bc3a65ee80ec00e51
Quentin Laparra <quentin.laparra@gmail.com>
2022-03-22 21:01:43 +0100

    add path login in market urls

--- user/models.py
+++ user/models.py
@@ -8,7 +8,6 @@ from user.managers import CustomUserManager
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(max_length=40)
-    profile_picture = models.ImageField(upload_to="userpicture/", blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
