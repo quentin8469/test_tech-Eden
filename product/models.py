b3a1dbd4b7f3f1767ded1a6a1bdf3adeb07a85e9
Quentin Laparra <quentin.laparra@gmail.com>
2022-03-22 22:55:10 +0100

    create product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    """ """
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    section = models.CharField(max_length=100)
+    reduction = models.DecimalField(max_digits=10, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return f"{self.name} - {self.price} - {self.section}"
