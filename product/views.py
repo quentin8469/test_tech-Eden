6a9235d9c98e7e0610e26883e4c3816021c64afe
Quentin Laparra <quentin.laparra@gmail.com>
2022-03-24 13:12:17 +0100

    WIP on product serializers and views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from product.models import Product
+from product.serializers import ProductSerializer
+
 
 # Create your views here.
+
+class ProductViewset(viewsets.ModelViewSet):
+    """ 
+    API end point that allows products to be wiev by authenticated users
+    """
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    #premission_classe = [permissions.IsAuthenticated,]
